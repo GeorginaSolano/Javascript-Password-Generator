aff00ac3263bd29136f3f74db3310a4f4c042269
GeorginaSolano <ggonzalezespinosa@gmail.com>
2021-12-04 22:10:24 +1100

    added code to create character set to be used for random selection

--- script.js
+++ script.js
@@ -1,6 +1,50 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  console.log("You clicked the button");
+  var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var charLower = "abcdefghijklmnopqrstuvwxyz";
+  var charNumeric = "0123456789";
+  var charSpecial = "!@#$%^&*()?";
+  var criteriaSet = " ";
+  var response = true;
+  response = window.confirm("would you like to include Uppercase characters?");
+  if (response === false) {
+     window.alert("Uppercase characters are not require.");
+    
+  } else {
+    criteriaSet += charUpper;
+  }
+  response = window.confirm("Would you like to include Lowercase characters?");
+  if (response === false) {
+    window.alert("Lowercase characters are not require.");
 
+  } else {
+    criteriaSet += charLower;
+  }
+  response = window.confirm("Would you like to include Numeric characters?");
+  if (response === false) {
+    window.alert("Numeric characters are not require.");
+  } else {
+    criteriaSet += charNumeric;
+  }
+  response = window.confirm("Would you like to include special characters?");
+  if (response === false) {
+    window.alert("Special characters are not require.");
+  } else {
+    criteriaSet += charSpecial;
+  }
+
+  console.log(criteriaSet);
+
+  // length = window.prompt("Please enter the length of the password.");
+  // if (length < 8 || length > 128) {
+  //   window.alert("Please enter a new value.");
+  // }
+     
+     
+  
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
