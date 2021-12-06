615814a4f709f3ad38ecfa8dbef36ff021d0fa45
GeorginaSolano <ggonzalezespinosa@gmail.com>
2021-12-06 12:46:42 +1100

    removed looking through generate password function

--- script.js
+++ script.js
@@ -6,51 +6,58 @@ function generatePassword() {
   var charLower = "abcdefghijklmnopqrstuvwxyz";
   var charNumeric = "0123456789";
   var charSpecial = "!@#$%^&*()?{}[]";
-  var criteriaSet = " ";
+  var passwordChars = " ";
   var response = true;
-  var passwordChar = " "
-
-  var passwordLength = window.prompt("Please enter the length of the password.");
-  if (passwordLength < 8 || passwordLength > 128) {
-    window.alert("Please enter a new value.");
+  var password = " ";
+  var passwordLength;
+  askLength();
+  function askLength() {
+    passwordLength= window.prompt("Please enter the length of the password.");
+    if (passwordLength < 8 || passwordLength > 128) {
+      window.alert("Please enter a new value.");
+      askLength();
+    }
   }
+  
 
   response = window.confirm("would you like to include Uppercase characters?");
   if (response === false) {
      window.alert("Uppercase characters are not require.");
     
   } else {
-    criteriaSet += charUpper;
+    passwordChars += charUpper;
   }
   response = window.confirm("Would you like to include Lowercase characters?");
   if (response === false) {
     window.alert("Lowercase characters are not require.");
 
   } else {
-    criteriaSet += charLower;
+    passwordChars += charLower;
   }
   response = window.confirm("Would you like to include Numeric characters?");
   if (response === false) {
     window.alert("Numeric characters are not require.");
   } else {
-    criteriaSet += charNumeric;
+    passwordChars += charNumeric;
   }
   response = window.confirm("Would you like to include special characters?");
   if (response === false) {
     window.alert("Special characters are not require.");
   } else {
-    criteriaSet += charSpecial;
+    passwordChars += charSpecial;
   } 
  
-  console.log(criteriaSet);
-  if (criteriaSet === " ") {
+  console.log(passwordChars);
+  if (passwordChars === " ") {
     window.alert("You must select characters to be included.Please try again");
+    generatePassword();
   }
 
 for (i=0; i < passwordLength; i++) {
-  passwordChar += criteriaSet[Math.floor(Math.random()*criteriaSet.length)];
+  password += passwordChars[Math.floor(Math.random()*passwordChars.length)];
 }
- console.log(passwordChar); 
+ console.log(password); 
+ return password;
 }
 // Write password to the #password input
 function writePassword() {
@@ -58,7 +65,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
